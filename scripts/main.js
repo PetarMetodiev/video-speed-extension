59f14dbc13b265d8324ef3591c642626dfe0deb9
Petar Metodiev <petmet@cobuilder.no>
2018-09-16 17:19:26 +0300

    Rename element id selector

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,18 @@
 const slider = document.getElementById('speed-slider');
 const sliderValueContainer = document.getElementById('speed-slider-value');
-const resultSpeedSlider = document.getElementById('result-speed-slider');
+const resultSpeedNumber = document.getElementById('result-speed-number');
 
-resultSpeedSlider.addEventListener('input', (ev) => {
+resultSpeedNumber.addEventListener('input', (ev) => {
 	slider.value = ev.target.value || 0;
 });
 
-resultSpeedSlider.addEventListener('blur', function (ev) {
-	if (!ev.target.value) {
-		this.value = slider.value;
-	}
+resultSpeedNumber.addEventListener('blur', (ev) => {
+	// Prevents values from outside slider range
+	resultSpeedNumber.value = slider.value;
 })
 
 slider.addEventListener('input', (ev) => {
-	resultSpeedSlider.value = ev.target.value;
+	resultSpeedNumber.value = ev.target.value;
 });
 
 slider.addEventListener('mouseup', (ev) => {
