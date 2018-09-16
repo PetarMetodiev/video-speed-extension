33b10523f272de030ef159dcee13bed18b17c5f4
Petar Metodiev <petmet@cobuilder.no>
2018-09-16 16:59:42 +0300

    Use input type number for displaying current value

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,20 @@ const slider = document.getElementById('speed-slider');
 const sliderValueContainer = document.getElementById('speed-slider-value');
 const resultSpeedSlider = document.getElementById('result-speed-slider');
 
-const currentValue = {
-	value: 0
-};
-
 resultSpeedSlider.addEventListener('input', (ev) => {
 	slider.value = ev.target.value || 0;
 });
 
+resultSpeedSlider.addEventListener('blur', function (ev) {
+	if (!ev.target.value) {
+		this.value = slider.value;
+	}
+})
+
 slider.addEventListener('input', (ev) => {
 	resultSpeedSlider.value = ev.target.value;
 });
+
+slider.addEventListener('mouseup', (ev) => {
+	console.log('Set video playback speed to: ', ev.target.value);
+})
